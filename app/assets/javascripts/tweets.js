9ae2c07a5fb47af8969f459ffc5dbde2f72b9b56
Klaus Fl <kfleerko@thoughtworks.com>
2014-08-07 21:29:18 +0200

    Fix bugs in the JS boilerplate - Ready for AJAXification!

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -1,16 +1,20 @@
-//Place all the behaviors and hooks related to the matching controller here.
-//All this logic will automatically be available in application.js.
-//You can use CoffeeScript in this file: http://coffeescript.org/
-
-$('form#new_tweet input[type="submit"').submit(function (e) {
-  e.preventDefault();
-
+var createTweetUsingAjax = function (e) {
+  var tweetToSend; // How do we get the new tweet to send?
   var params = {}; // What might need to be in this object?
-  $.post('<%= create_tweet_path %>', params())
+
+  $.post('/tweets', params)
   .done(function (html) {
     alert('Got response: ' + html); // What do we need to do here?
   })
   .error(function (error) {
-    alert(error); // And here?
+    alert('Got error:' + error.responseText); // And here?
+  });
+};
+
+// Here is some set-up - remove the // to start working on AJAX!
+$(function () {
+  $('form#new_tweet').submit(function () {
+    //createTweetUsingAjax();
+    //e.preventDefault();
   });
 });
